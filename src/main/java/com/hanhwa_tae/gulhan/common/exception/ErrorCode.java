ee789359f21e2903a2e89d72d79d36c23f4ca82c
Cho-Hyun-Seung <toki0327@naver.com>
2025-04-07 15:27:01 +0900

    feat: 에러코드 생성 및 유저 에러코드 기입 #24

---
+++ src/main/java/com/hanhwa_tae/gulhan/common/exception/ErrorCode.java
@@ -0,0 +1,29 @@
+package com.hanhwa_tae.gulhan.common.exception;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+public enum ErrorCode {
+    // 회원
+    USER_NOT_FOUND("10001", "아이디 또는 비밀번호가 잘못되었습니다.", HttpStatus.UNAUTHORIZED),  // 401
+    REFRESH_TOKEN_EXPIRATION("10002", "로그인이 만료되었습니다.", HttpStatus.NOT_FOUND),  // 404
+    ACCESS_DENIED("10003", "해당 페이지에 대한 조회 권한이 없습니다.", HttpStatus.FORBIDDEN),    // 403
+
+
+    // TODO 나머지 이어서 적어주세요!
+
+
+
+    // 그 외 기타 오류
+    VALIDATION_ERROR("50000", "입력 값 검증 오류입니다.", HttpStatus.BAD_REQUEST),
+    INTERNAL_SERVER_ERROR("50001", "내부 서버 오류 입니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+
+
+    private final String code;
+    private final String message;
+    private final HttpStatus httpStatus;
+}
