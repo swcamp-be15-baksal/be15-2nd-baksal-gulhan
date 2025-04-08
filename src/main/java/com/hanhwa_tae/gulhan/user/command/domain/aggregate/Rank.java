42521371d5620568fc2ad83b34a50000a07bf50e
Cho-Hyun-Seung <toki0327@naver.com>
2025-04-08 19:08:19 +0900

    Refactor: Rank Getter 추가

--- src/main/java/com/hanhwa_tae/gulhan/user/command/domain/aggregate/Rank.java
+++ src/main/java/com/hanhwa_tae/gulhan/user/command/domain/aggregate/Rank.java
@@ -1,9 +1,11 @@
 package com.hanhwa_tae.gulhan.user.command.domain.aggregate;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 
 
 @Entity
+@Getter
 @Table(name = "rank")
 public class Rank {
 
