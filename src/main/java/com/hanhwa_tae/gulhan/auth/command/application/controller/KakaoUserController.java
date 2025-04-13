5ed2c24acb4e27117808cfb6254eda176db38747
lddocy <jkcy911219@naver.com>
2025-04-13 19:25:19 +0900

    Fix: 카카오 Swagger 추가 작성

--- src/main/java/com/hanhwa_tae/gulhan/auth/command/application/controller/KakaoUserController.java
+++ src/main/java/com/hanhwa_tae/gulhan/auth/command/application/controller/KakaoUserController.java
@@ -49,8 +49,7 @@ public class KakaoUserController {
     }
 
     // 카카오 로그인 콜백 처리 (인가 코드로 로그인 요청)
-    // TODO 뭐라 적어야 하죠?
-    @Operation(summary = "?????", description = "???")
+    @Operation(summary = "카카오 엑세스 토큰 요청", description = "회원은 카카오 로그인에 성공 시 인가 코드로 토큰을 발급 받을 수 있다.")
     @GetMapping("/callback")
     public ResponseEntity<ApiResponse<KakaoLoginResponse>> kakaoCallback(@RequestParam("code") String code) {
         log.info("카카오 로그인 요청 시작: code={}", code);
