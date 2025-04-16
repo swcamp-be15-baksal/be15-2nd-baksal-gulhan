3bf7c615579e0bf9787db1735127865d3fda40fc
Cho-Hyun-Seung <toki0327@naver.com>
2025-04-16 17:14:18 +0900

    test: 유저 로그인 성공&실패 테스트 #133

---
+++ second-server/src/test/java/com/hanhwa_tae/secondserver/auth/query/service/AuthQueryServiceImplTest.java
@@ -0,0 +1,130 @@
+package com.hanhwa_tae.secondserver.auth.query.service;
+
+import com.hanhwa_tae.secondserver.auth.command.application.dto.response.TokenResponse;
+import com.hanhwa_tae.secondserver.auth.command.domain.repository.AuthRepository;
+import com.hanhwa_tae.secondserver.auth.query.dto.request.LoginRequest;
+import com.hanhwa_tae.secondserver.common.exception.BusinessException;
+import com.hanhwa_tae.secondserver.common.exception.ErrorCode;
+import com.hanhwa_tae.secondserver.user.command.domain.aggregate.*;
+import com.hanhwa_tae.secondserver.user.query.mapper.UserMapper;
+import com.hanhwa_tae.secondserver.utils.jwt.JwtTokenProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class AuthQueryServiceImplTest {
+
+    @Mock
+    private UserMapper userMapper;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Mock
+    private AuthRepository authRepository;
+
+    @InjectMocks
+    private AuthQueryServiceImpl authQueryService;
+
+    private final String userId = "user99";
+    private final String wrongId = "fail99";
+    private final String password = "pass99";
+    private final String wrongPasssword = "fail00";
+    private final String encodedPassword = "encode99";
+    private final String email = "toki0327@naver.com";
+    private final String accessToken = "accessToken";
+    private final String refreshToken = "refreshToken";
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = User.builder()
+                .userId(userId)
+                .email(email)
+                .gender(GenderType.M)
+                .loginType(LoginType.GENERAL)
+                .password(encodedPassword)
+                .rank(Rank.builder()
+                        .rankName(RankType.COMMONER)
+                        .pointRate(1)
+                        .build())
+                .build();
+    }
+
+    @Test
+    @DisplayName("로그인 성공 테스트")
+    void testLoginSuccess() {
+        LoginRequest request = LoginRequest.builder()
+                .userId(userId)
+                .password(password)
+                .build();
+        /* given */
+//         미리 만들어진 유저 정보가 return 되도록 설정
+        when(userMapper.findUserByUserId(request.getUserId())).thenReturn(Optional.of(user));
+
+        /* 비밀번호를 비교할 때 true가 return 되도록 설정 */
+        when(passwordEncoder.matches(password, encodedPassword)).thenReturn(true);
+
+        /* 엑세스 토큰 생성 시 생성될 토큰 설정*/
+        when(jwtTokenProvider.createAccessToken(any(),any(), any())).thenReturn(accessToken);
+
+        /* 리프레쉬 토큰 생성 시 생성될 토큰 설정*/
+        when(jwtTokenProvider.createRefreshToken(any(), any(), any())).thenReturn(refreshToken);
+
+        /* when */
+        TokenResponse response = authQueryService.login(request);
+
+        /* then */
+        assertEquals(response.getAccessToken(), accessToken);
+        assertEquals(response.getRefreshToken(), refreshToken);
+    }
+
+    @Test
+    @DisplayName("아이디가 틀린 경우")
+    void testWrongIdLogin(){
+        LoginRequest request = LoginRequest.builder()
+                .userId(wrongId)
+                .password(password)
+                .build();
+
+        BusinessException exception = assertThrows(BusinessException.class,
+                () -> authQueryService.login(request));
+
+        assertEquals(ErrorCode.INVALID_LOGIN_REQUEST, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("비밀번호가 틀린 경우")
+    void testWrongPasswordLogin(){
+        LoginRequest request = LoginRequest.builder()
+                .userId(userId)
+                .password(wrongPasssword)
+                .build();
+
+        BusinessException exception = assertThrows(BusinessException.class,
+                () -> authQueryService.login(request));
+
+        assertEquals(ErrorCode.INVALID_LOGIN_REQUEST, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("Access Token 재발급")
+    void testReissueToken(){
+
+    }
+}
