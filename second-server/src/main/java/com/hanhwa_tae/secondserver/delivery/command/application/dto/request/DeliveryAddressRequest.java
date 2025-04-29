9b487f1988f7f4192b9452645ef13db8d865a57a
lddocy <jkcy911219@naver.com>
2025-04-29 21:44:18 +0900

    Feat: 기본 배송지 설정, 배송지 수정 기능 수정 #154

--- second-server/src/main/java/com/hanhwa_tae/secondserver/delivery/command/application/dto/request/DeliveryAddressRequest.java
+++ second-server/src/main/java/com/hanhwa_tae/secondserver/delivery/command/application/dto/request/DeliveryAddressRequest.java
@@ -2,7 +2,6 @@ package com.hanhwa_tae.secondserver.delivery.command.application.dto.request;
 
 import com.hanhwa_tae.secondserver.delivery.command.domain.aggregate.DefaultAddress;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Getter;
 
