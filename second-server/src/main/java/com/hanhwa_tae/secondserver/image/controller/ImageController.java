f27668b5f8402eb8a3632263981643ece0e8d99f
Cho-Hyun-Seung <toki0327@naver.com>
2025-04-29 17:15:00 +0900

    feat: 이미지 cloudfront url 응답 #147

--- second-server/src/main/java/com/hanhwa_tae/secondserver/image/controller/ImageController.java
+++ second-server/src/main/java/com/hanhwa_tae/secondserver/image/controller/ImageController.java
@@ -1,11 +1,13 @@
 package com.hanhwa_tae.secondserver.image.controller;
 
 import com.hanhwa_tae.secondserver.common.dto.ApiResponse;
+import com.hanhwa_tae.secondserver.image.dto.response.ImageUploadResponse;
 import com.hanhwa_tae.secondserver.image.service.ImageService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -19,13 +21,13 @@ public class ImageController {
 
     // TODO : 이미지 넣기
     @Operation(summary = "이미지 넣기", description = "이미지 삽입")
-    @PutMapping()
-    public ResponseEntity<ApiResponse<Void>> uploadImage(
+    @PostMapping()
+    public ResponseEntity<ApiResponse<ImageUploadResponse>> uploadImage(
         @RequestPart("image") MultipartFile file
     ) throws Exception {
-        imageService.uploadImage(file);
+        ImageUploadResponse response = imageService.uploadImage(file);
 
-        return ResponseEntity.ok(ApiResponse.success(null));
+        return ResponseEntity.ok(ApiResponse.success(response));
     }
 
 
