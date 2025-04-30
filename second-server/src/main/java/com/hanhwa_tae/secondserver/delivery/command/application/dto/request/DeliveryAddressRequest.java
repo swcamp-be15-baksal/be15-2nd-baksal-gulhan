a5d9befa030a0c931cdd889a50263989c55543f7
lddocy <jkcy911219@naver.com>
2025-04-30 09:34:00 +0900

    Fix: 배송지 요청 클래스에 어노테이션 추가

--- second-server/src/main/java/com/hanhwa_tae/secondserver/delivery/command/application/dto/request/DeliveryAddressRequest.java
+++ second-server/src/main/java/com/hanhwa_tae/secondserver/delivery/command/application/dto/request/DeliveryAddressRequest.java
@@ -1,6 +1,7 @@
 package com.hanhwa_tae.secondserver.delivery.command.application.dto.request;
 
 import com.hanhwa_tae.secondserver.delivery.command.domain.aggregate.DefaultAddress;
+import com.hanhwa_tae.secondserver.user.annotation.ValidPhone;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,6 +14,7 @@ public class DeliveryAddressRequest {
     @NotNull
     private String receiver;
     @NotNull
+    @ValidPhone
     private String receiverPhone;
     @NotNull
     private DefaultAddress defaultAddress;
