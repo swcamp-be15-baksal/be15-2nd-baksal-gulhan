a290f023e041ec662b982d5f54d57517c08d7372
dxmlk <1101sky@korea.ac.kr>
2025-05-07 22:45:18 +0900

    feat: 텍스트 에디터 이미지 업로드 기능

--- first-server/src/main/java/com/hanhwa_tae/firstserver/client/UserClient.java
+++ first-server/src/main/java/com/hanhwa_tae/firstserver/client/UserClient.java
@@ -5,11 +5,15 @@ import com.hanhwa_tae.firstserver.config.FeignClientConfig;
 import com.hanhwa_tae.firstserver.feignexample.dto.response.UserInfoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "auth-server", configuration = FeignClientConfig.class)
 public interface UserClient {
 
     @GetMapping("/users/info")
     ApiResponse<UserInfoResponse> getUserInfo();
+
+    @PostMapping("/image/save")
+    ApiResponse<Void> saveImage(SaveImageRequest request);
 
 }
