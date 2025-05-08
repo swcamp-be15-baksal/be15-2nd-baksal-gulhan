672ec46cd507f054f0d152d3dc86adef6b752d03
hwan1023 <qpzmrlaxoghks@naver.com>
2025-05-08 14:39:06 +0900

    충돌 수정

--- first-server/src/main/java/com/hanhwa_tae/firstserver/payment/query/dto/response/OrderHistoriesDetailResponse.java
+++ first-server/src/main/java/com/hanhwa_tae/firstserver/payment/query/dto/response/OrderHistoriesDetailResponse.java
@@ -2,9 +2,10 @@ package com.hanhwa_tae.firstserver.payment.query.dto.response;
 
 
 import lombok.Builder;
+import lombok.Getter;
 
 import java.util.List;
-
+@Getter
 @Builder
 public class OrderHistoriesDetailResponse {
     private List<OrderHistoriesResponse> orderHistories;
--- first-server/src/main/java/com/hanhwa_tae/firstserver/payment/query/dto/response/OrderHistoriesResponse.java
+++ first-server/src/main/java/com/hanhwa_tae/firstserver/payment/query/dto/response/OrderHistoriesResponse.java
@@ -2,15 +2,22 @@ package com.hanhwa_tae.firstserver.payment.query.dto.response;
 
 import com.hanhwa_tae.firstserver.cart.command.domain.aggregate.Confirm;
 import com.hanhwa_tae.firstserver.common.domain.TargetType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
-
+@Getter
+@NoArgsConstructor  // 기본 생성자 추가
+@AllArgsConstructor // 선택적으로 모든 필드를 받는 생성자 추가 (Optional)
+@Builder
 public class OrderHistoriesResponse {
     private TargetType orderHistoryType;
     private int targetId;
     private int quantity;
-    private int orderId;
-    private Timestamp orderAt;
+    private String orderId;
+    private Timestamp orderedAt;
     private Confirm isConfirmed;
     private int price;
 }
